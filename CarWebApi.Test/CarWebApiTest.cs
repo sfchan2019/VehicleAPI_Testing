033a6f61a27ea0955671dc12473e6450c0f68b3b
DESKTOP-1ORJFAV\Sing <sing840722@gmail.com>
2019-10-07 21:42:16 +0100

    Commit from desktop

---
+++ CarWebApi.Test/CarWebApiTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using CarWebApi;
+using Newtonsoft.Json.Linq;
+
+namespace CarWebApi.Test
+{
+    [TestFixture]
+    public class CarWebApiTest
+    {
+        SingleCarApiRequest carApiRequest;
+        public CarWebApiTest()
+        {
+            carApiRequest = new SingleCarApiRequest();
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            carApiRequest.RequestVehicleTypesForMake("mercedes");
+            Assert.AreEqual("Response returned successfully", carApiRequest.SingleResponseObject["Message"].ToString());
+
+            carApiRequest.RequestVehicleTypesForMake("Honda");
+            Assert.AreEqual("Response returned successfully", carApiRequest.SingleResponseObject["Message"].ToString());
+        }
+    }
+}
---
+++ CarWebApi/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+
+namespace CarWebApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class SingleCarApiRequest
+    {
+        public RestClient Client { get; set; }
+        private string base_url;
+        public JObject SingleResponseObject { get; set; }
+
+        public SingleCarApiRequest()
+        {
+            Client = new RestClient();
+            Client.BaseUrl = new Uri("https://vpic.nhtsa.dot.gov/api/vehicles");
+        }
+
+        //mercedes
+        public void RequestVehicleTypesForMake(string brand)
+        {
+            brand = brand.ToLower();
+            RestRequest request = new RestRequest();
+            request.AddHeader("Content-Type", "application/json");
+            request.Resource = $"/GetVehicleTypesForMake{brand}?format=json";
+            IRestResponse response = Client.Execute(request);
+            SingleResponseObject = JObject.Parse(response.Content);
+        }
+    }
+}
